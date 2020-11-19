f726f71ca78527c54cb35d3b3271157df4316825
Glitch (hello-express) <none>
2020-11-19 22:08:07 +0000

    🍉🏥 Checkpoint ./data.js:8045597/1063 ./data/test.js:8045597/106 ./data/test2.js:8045597/34 ./index.js:8045597/889 ./index.ejs:8045597/29

--- data.js
+++ data.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 
-const files = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const files = fs.readdirSync("./data").filter(file => file.endsWith(".js"));
 
 var i = 0;
-var obj = {}
-while(i < files.length){
-  obj[files[i]] = require(`./`)
+var obj = {};
+while (i < files.length) {
+  obj[files[i].replace(".js", "")] = require(`./data/${files[i]}`);
+  i++;
 }
+module.exports = obj;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const config = require("./config.js")
-const ejs = require('ejs');
+const config = require("./config.js");
+const ejs = require("ejs");
+const fs = require('fs'); 
 
-
-var data = ""
-
+var data = require("./data.js");
+var res = ejs.render(fs.readFileSync("index.ejs", "utf8"), data);
+console.log(res)
