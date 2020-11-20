19e9bb4d9f9f2234b376df384193407f5d5c1346
Glitch (gitme) <none>
2020-11-20 01:27:44 +0000

    add golang to icons

--- data/icons.js
+++ data/icons.js
@@ -5,5 +5,7 @@ module.exports = {
   python:
     "https://cdn.glitch.com/17eaef8d-c248-49b5-81da-45e23cdc0b12%2Ficons8-python-48.png?v=1605830851549",
   markdown:
-    "https://cdn.glitch.com/17eaef8d-c248-49b5-81da-45e23cdc0b12%2Ficons8-markdown-24.png?v=1605831028200"
+    "https://cdn.glitch.com/17eaef8d-c248-49b5-81da-45e23cdc0b12%2Ficons8-markdown-24.png?v=1605831028200",
+  golang:
+    "https://cdn.glitch.com/17eaef8d-c248-49b5-81da-45e23cdc0b12%2Ficons8-golang-48.png?v=1605835614356"
 };
