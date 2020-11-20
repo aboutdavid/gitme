8bcf194b092a7711071f7363d1ee077d95f00908
Glitch (gitme) <none>
2020-11-20 00:53:05 +0000

    🚏🍩 Checkpoint ./index.js:8045597/6550 ./package.json:8045597/1443

--- index.js
+++ index.js
@@ -2,39 +2,52 @@ var startTime = new Date().valueOf();
 const config = require("./config.js");
 const ejs = require("ejs");
 const fs = require("fs");
+const nunjucks = require("nunjucks");
+const core = require("@actions/core");
+const GITHUB_TOKEN = core.getInput("gh_token");
 const md = require("markdown-it")({
   html: true,
   linkify: true,
   typographer: true
 });
-const nunjucks = require("nunjucks")
 
-console.log("=== GitME, the README.md generator for GitHub ===")
-console.log("Version: 0.0.1")
-console.log(`Reading ${config.renderFile}...`)
+console.log("=== GitME, the README.md generator for GitHub ===");
+console.log("Version: 0.0.1");
+console.log(`Reading ${config.renderFile}...`);
 
 // Read the file
-var str = fs.readFileSync(config.renderFile, "utf8")
+var str = fs.readFileSync(config.renderFile, "utf8");
 
 // We will require our data file, which scans our directory and compiles all of the .js files into one onject
-console.log("Compiling data...")
+console.log("Compiling data...");
 var data = require("./data.js");
-console.log(`Rendering ${config.renderFile} with ${config.renderEngine}`)
+console.log(`Rendering ${config.renderFile} with ${config.renderEngine}`);
 
 // Render ejs
-if(config.renderEngine =="ejs"){
-var res = ejs.render(str, data);
-} else if(config.renderEngine =="nunjucks"){
+if (config.renderEngine == "ejs") {
+  var res = ejs.render(str, data);
+} else if (config.renderEngine == "nunjucks") {
   var res = nunjucks.renderString(str, data);
 } else {
-  throw new Error(`${config.renderEngine} is not a valid rendering engine. Please see (inserturlhere) to see valid rendering engines.`);
+  throw new Error(
+    `${config.renderEngine} is not a valid rendering engine. Please see (inserturlhere) to see valid rendering engines.`
+  );
   process.exit(1);
 }
 
 // Render Markdown
-console.log("Rendering Markdown...")
+console.log("Rendering Markdown...");
 res = md.render(res);
 
 // Save .md file
 fs.writeFileSync("README.md", res);
-console.log(`GitME compilied your (awesome) README.md file in ${( new Date().valueOf() - startTime) / 1000} seconds!`)
+
+console.log("Saving the README.md file to");
+require("./run.js").command("git", ["add", "READMe"])
+require("./run.js").command("git", ["commit", "-m", "GitME compiled your README.md file!"]);
+
+console.log(
+  `GitME compilied your (awesome) README.md file in ${(new Date().valueOf() -
+    startTime) /
+    1000} seconds!`
+);
